77112ebcf1c65b74e08d16ad9808a2ffaaafe3da
Benno <uth.benno@gmail.com>
2024-03-20 10:13:25 +0100

    Update process_ontonotes_data.sh script

--- scripts/process_ontonotes_data.sh
+++ scripts/process_ontonotes_data.sh
@@ -5,10 +5,10 @@ for LANG in english arabic chinese
 do
     echo === Processing $LANG ===
     mkdir -p data/ontonotes5/processed/$LANG
-    python -m ml.cmd.convert_ontonotes_data  data/ontonotes5/processed/$LANG/\ $ONTO_PATH/data/files/data/$LANG
+    python -m ml.cmd.convert_ontonotes_data data/ontonotes5/data/files/data/$LANG data/ontonotes5/processed/english/
     --as-sentences --is-complete
 
     mkdir -p data/ontonotes5/processed_docs/$LANG
-    python -m ml.cmd.convert_ontonotes_data  data/ontonotes5/processed_docs/$LANG/\ $ONTO_PATH/data/files/data/$LANG
+    python -m ml.cmd.convert_ontonotes_data $ONTO_PATH/data/files/data/$LANG data/ontonotes5/processed_docs/$LANG/\
     --is-complete
 done
