1deb2b6f3031b59f2bfa0bee70744b5782e073f9
Benno <uth.benno@gmail.com>
2024-03-20 15:05:35 +0100

    Update paths for HuggingFace models in make_vocabs.sh

--- scripts/make_vocabs.sh
+++ scripts/make_vocabs.sh
@@ -2,6 +2,6 @@
 
 # python -m ml.cmd.make_vocab roberta-base data/conll2003/eng/entity.train.jsonl data/conll2003/roberta-entity.vocab
 # python -m ml.cmd.make_vocab bert-base-multilingual-cased data/conll2003/deu/entity.train.jsonl data/conll2003/mbert-entity.vocab
-python -m ml.cmd.make_vocab /gpfsdswork/dataset/roberta-base data/ontonotes5/processed/english/train.jsonl data/ontonotes5/processed/roberta-entity.vocab
-python -m ml.cmd.make_vocab /gpfsdswork/dataset/bert-base-multilingual-cased data/ontonotes5/processed/chinese/train.jsonl data/ontonotes5/processed/mbert-entity.vocab
+python -m ml.cmd.make_vocab /gpfsdswork/dataset/HuggingFace_Models/roberta-base data/ontonotes5/processed/english/train.jsonl data/ontonotes5/processed/roberta-entity.vocab
+python -m ml.cmd.make_vocab /gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased data/ontonotes5/processed/chinese/train.jsonl data/ontonotes5/processed/mbert-entity.vocab
 
