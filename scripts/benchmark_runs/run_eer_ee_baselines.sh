6bd976fbba47839e144e7b8ddc37852fbe595d07
buthaya <buthayasooriyar@scor.com>
2024-04-09 00:11:36 +0200

    Debug

--- scripts/benchmark_runs/run_eer_ee_baselines.sh
+++ scripts/benchmark_runs/run_eer_ee_baselines.sh
@@ -16,6 +16,37 @@ ENTITY_RATIO_MARGIN=0.05
 
 
 # # Conll english
+# tacl-eer_eng-c_linear_raw-short
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=eng-c\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/conll2003/eng\
+ TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=entity.dev-docs.jsonl\
+ TEST_DATA=entity.test-docs.jsonl\
+ VOCAB_PATH=data/conll2003/roberta-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/roberta-base\
+ PAD_TOKEN="<pad>"\
+ OOV_TOKEN="<unk>"\
+ BATCH_SIZE=15\
+ VALIDATION_BATCH_SIZE=15\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_TYPE=$PRIOR_TYPE\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+#  PRIOR_TYPE=null\
+
+# # Conll english
 # # tacl-eer_eng-c_ee_eer
 # bash scripts/run_remote_ml_experiment.sh\
 #  PUBLIC_IP=3.238.238.107\
@@ -211,32 +242,32 @@ ENTITY_RATIO_MARGIN=0.05
 
 # Ontonotes arabic
 # tacl-eer_ara_ee_eer
-bash scripts/run_remote_ml_experiment.sh\
- PUBLIC_IP=3.236.139.35\
- PRIVATE_IP=172.31.59.7\
- RUN_JUPYTER=$RUN_JUPYTER\
- RUN_TENSORBOARD=$RUN_TENSORBOARD\
- LANG_LABEL=ara\
- DATASET_LABEL=$DATASET_LABEL\
- METHOD_LABEL=$METHOD_LABEL\
- BASE_CONFIG=experiments/supervised_tagger.jsonnet\
- ASSUME_COMPLETE=$ASSUME_COMPLETE\
- LANG_DIR=data/ontonotes5/processed_docs/arabic\
- TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
- DEV_DATA=dev.jsonl\
- TEST_DATA=test.jsonl\
- VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
- MODEL_NAME=bert-base-multilingual-cased\
- PAD_TOKEN="[PAD]"\
- OOV_TOKEN="[UNK]"\
- BATCH_SIZE=2\
- VALIDATION_BATCH_SIZE=1\
- RANDOM_SEED=$RANDOM_SEED\
- DROPOUT=$DROPOUT\
- LR=$LR\
- NUM_EPOCHS=30\
- PRIOR_TYPE=$PRIOR_TYPE\
- PRIOR_WEIGHT=$PRIOR_WEIGHT\
- ENTITY_RATIO=$ENTITY_RATIO\
- ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+# bash scripts/run_remote_ml_experiment.sh\
+#  PUBLIC_IP=3.236.139.35\
+#  PRIVATE_IP=172.31.59.7\
+#  RUN_JUPYTER=$RUN_JUPYTER\
+#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
+#  LANG_LABEL=ara\
+#  DATASET_LABEL=$DATASET_LABEL\
+#  METHOD_LABEL=$METHOD_LABEL\
+#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
+#  LANG_DIR=data/ontonotes5/processed_docs/arabic\
+#  TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
+#  DEV_DATA=dev.jsonl\
+#  TEST_DATA=test.jsonl\
+#  VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
+#  MODEL_NAME=bert-base-multilingual-cased\
+#  PAD_TOKEN="[PAD]"\
+#  OOV_TOKEN="[UNK]"\
+#  BATCH_SIZE=2\
+#  VALIDATION_BATCH_SIZE=1\
+#  RANDOM_SEED=$RANDOM_SEED\
+#  DROPOUT=$DROPOUT\
+#  LR=$LR\
+#  NUM_EPOCHS=30\
+#  PRIOR_TYPE=$PRIOR_TYPE\
+#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
+#  ENTITY_RATIO=$ENTITY_RATIO\
+#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
 
