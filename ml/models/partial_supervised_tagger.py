93a33ff774f064e8702ea5524d36ab562c6f7962
buthaya <buthayasooriyar@scor.com>
2024-04-09 10:36:04 +0200

    Debug

--- ml/models/partial_supervised_tagger.py
+++ ml/models/partial_supervised_tagger.py
@@ -117,7 +117,7 @@ class PartialSupervisedTagger(Model):
         initializer: InitializerApplicator = InitializerApplicator(),
         # Losses
         prior_loss_weight: float = 1.0, # lambda parameter 
-        prior_type: str = None,
+        prior_loss_type: str = None,
         entity_ratio: float = 0.2,
         entity_ratio_margin: float = 0.05,
         regularizer: Optional[RegularizerApplicator] = None,
@@ -156,7 +156,7 @@ class PartialSupervisedTagger(Model):
 
         initializer(self)
 
-        self.prior_type = None
+        self.prior_loss_type = None
         self.prior_loss_weight = prior_loss_weight
         self.entity_ratio = entity_ratio
         self.entity_ratio_margin = entity_ratio_margin
