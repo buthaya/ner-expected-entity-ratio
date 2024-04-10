ed96905faa1d4476879b2ba36cf46969bead6f26
buthaya <buthayasooriyar@scor.com>
2024-04-10 15:34:53 +0200

    Test EER NNS with BATCH_SIZE 15

--- scripts/benchmark_runs/run_eer_nns_baselines.sh
+++ scripts/benchmark_runs/run_eer_nns_baselines.sh
@@ -39,25 +39,56 @@ ENTITY_RATIO_MARGIN=0.05
 
 # Conll english
 # tacl-eer_eng-c_nns_eer
+# bash scripts/run_ml_experiment.sh\
+#  IS_REMOTE=$IS_REMOTE\
+#  RUN_JUPYTER=$RUN_JUPYTER\
+#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
+#  LANG_LABEL=eng-c\
+#  DATASET_LABEL=$DATASET_LABEL\
+#  METHOD_LABEL=$METHOD_LABEL\
+#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
+#  LANG_DIR=data/conll2003/eng\
+#  TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
+#  DEV_DATA=entity.dev-docs.jsonl\
+#  TEST_DATA=entity.test-docs.jsonl\
+#  VOCAB_PATH=data/conll2003/roberta-entity.vocab\
+#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/roberta-base\
+#  PAD_TOKEN="<pad>"\
+#  OOV_TOKEN="<unk>"\
+#  BATCH_SIZE=15\
+#  VALIDATION_BATCH_SIZE=15\
+#  RANDOM_SEED=$RANDOM_SEED\
+#  DROPOUT=$DROPOUT\
+#  LR=$LR\
+#  NUM_EPOCHS=$NUM_EPOCHS\
+#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
+#  ENTITY_RATIO=$ENTITY_RATIO\
+#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+# #  PRIOR_TYPE=$PRIOR_TYPE 
+
+
+# Conll german
+# tacl-eer_deu_nns_eer
 bash scripts/run_ml_experiment.sh\
  IS_REMOTE=$IS_REMOTE\
  RUN_JUPYTER=$RUN_JUPYTER\
  RUN_TENSORBOARD=$RUN_TENSORBOARD\
- LANG_LABEL=eng-c\
+ LANG_LABEL=deu\
  DATASET_LABEL=$DATASET_LABEL\
  METHOD_LABEL=$METHOD_LABEL\
  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
  ASSUME_COMPLETE=$ASSUME_COMPLETE\
- LANG_DIR=data/conll2003/eng\
+ LANG_DIR=data/conll2003/deu\
  TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
  DEV_DATA=entity.dev-docs.jsonl\
  TEST_DATA=entity.test-docs.jsonl\
- VOCAB_PATH=data/conll2003/roberta-entity.vocab\
- MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/roberta-base\
- PAD_TOKEN="<pad>"\
- OOV_TOKEN="<unk>"\
- BATCH_SIZE=14\
- VALIDATION_BATCH_SIZE=14\
+ VOCAB_PATH=data/conll2003/mbert-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
+ PAD_TOKEN="[PAD]"\
+ OOV_TOKEN="[UNK]"\
+ BATCH_SIZE=15\
+ VALIDATION_BATCH_SIZE=15\
  RANDOM_SEED=$RANDOM_SEED\
  DROPOUT=$DROPOUT\
  LR=$LR\
@@ -65,188 +96,157 @@ bash scripts/run_ml_experiment.sh\
  PRIOR_WEIGHT=$PRIOR_WEIGHT\
  ENTITY_RATIO=$ENTITY_RATIO\
  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
-#  PRIOR_TYPE=$PRIOR_TYPE 
-
-
-# # Conll german
-# # tacl-eer_deu_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=deu\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/conll2003/deu\
-#  TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=entity.dev-docs.jsonl\
-#  TEST_DATA=entity.test-docs.jsonl\
-#  VOCAB_PATH=data/conll2003/mbert-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
-#  PAD_TOKEN="[PAD]"\
-#  OOV_TOKEN="[UNK]"\
-#  BATCH_SIZE=14\
-#  VALIDATION_BATCH_SIZE=14\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
-# #  PRIOR_TYPE=$PRIOR_TYPE
-
-
-# # Conll spanish
-# # tacl-eer_esp_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=esp\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/conll2003/esp\
-#  TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=entity.dev-docs.jsonl\
-#  TEST_DATA=entity.test-docs.jsonl\
-#  VOCAB_PATH=data/conll2003/mbert-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
-#  PAD_TOKEN="[PAD]"\
-#  OOV_TOKEN="[UNK]"\
-#  BATCH_SIZE=14\
-#  VALIDATION_BATCH_SIZE=14\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
-# #  PRIOR_TYPE=$PRIOR_TYPE
-
-# # Conll dutch
-# # tacl-eer_ned_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=ned\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/conll2003/ned\
-#  TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=entity.dev-docs.jsonl\
-#  TEST_DATA=entity.test-docs.jsonl\
-#  VOCAB_PATH=data/conll2003/mbert-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
-#  PAD_TOKEN="[PAD]"\
-#  OOV_TOKEN="[UNK]"\
-#  BATCH_SIZE=14\
-#  VALIDATION_BATCH_SIZE=14\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
-# #  PRIOR_TYPE=$PRIOR_TYPE
-
-# # Ontonotes english
-# # tacl-eer_eng-o_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=eng-o\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/ontonotes5/processed_docs/english\
-#  TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=dev.jsonl\
-#  TEST_DATA=test.jsonl\
-#  VOCAB_PATH=data/ontonotes5/processed_docs/roberta-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/roberta-base\
-#  PAD_TOKEN="<pad>"\
-#  OOV_TOKEN="<unk>"\
-#  BATCH_SIZE=2\
-#  VALIDATION_BATCH_SIZE=1\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
 #  PRIOR_TYPE=$PRIOR_TYPE
 
 
-# # Ontonotes chinese - intially had 30 epochs
-# # tacl-eer_chi_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=chi\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/ontonotes5/processed_docs/chinese\
-#  TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=dev.jsonl\
-#  TEST_DATA=test.jsonl\
-#  VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
-#  PAD_TOKEN="[PAD]"\
-#  OOV_TOKEN="[UNK]"\
-#  BATCH_SIZE=2\
-#  VALIDATION_BATCH_SIZE=1\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
-# #  PRIOR_TYPE=$PRIOR_TYPE
+# Conll spanish
+# tacl-eer_esp_nns_eer
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=esp\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/conll2003/esp\
+ TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=entity.dev-docs.jsonl\
+ TEST_DATA=entity.test-docs.jsonl\
+ VOCAB_PATH=data/conll2003/mbert-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
+ PAD_TOKEN="[PAD]"\
+ OOV_TOKEN="[UNK]"\
+ BATCH_SIZE=15\
+ VALIDATION_BATCH_SIZE=15\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+#  PRIOR_TYPE=$PRIOR_TYPE
 
-# # Ontonotes arabic initially had 30 epochs
-# # tacl-eer_ara_nns_eer
-# bash scripts/run_ml_experiment.sh\
-#  IS_REMOTE=$IS_REMOTE\
-#  RUN_JUPYTER=$RUN_JUPYTER\
-#  RUN_TENSORBOARD=$RUN_TENSORBOARD\
-#  LANG_LABEL=ara\
-#  DATASET_LABEL=$DATASET_LABEL\
-#  METHOD_LABEL=$METHOD_LABEL\
-#  BASE_CONFIG=experiments/supervised_tagger.jsonnet\
-#  ASSUME_COMPLETE=$ASSUME_COMPLETE\
-#  LANG_DIR=data/ontonotes5/processed_docs/arabic\
-#  TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
-#  DEV_DATA=dev.jsonl\
-#  TEST_DATA=test.jsonl\
-#  VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
-#  MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
-#  PAD_TOKEN="[PAD]"\
-#  OOV_TOKEN="[UNK]"\
-#  BATCH_SIZE=2\
-#  VALIDATION_BATCH_SIZE=1\
-#  RANDOM_SEED=$RANDOM_SEED\
-#  DROPOUT=$DROPOUT\
-#  LR=$LR\
-#  NUM_EPOCHS=$NUM_EPOCHS\
-#  PRIOR_TYPE=$PRIOR_TYPE\
-#  PRIOR_WEIGHT=$PRIOR_WEIGHT\
-#  ENTITY_RATIO=$ENTITY_RATIO\
-#  ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+# Conll dutch
+# tacl-eer_ned_nns_eer
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=ned\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/conll2003/ned\
+ TRAIN_DATA=entity.train-docs$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=entity.dev-docs.jsonl\
+ TEST_DATA=entity.test-docs.jsonl\
+ VOCAB_PATH=data/conll2003/mbert-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
+ PAD_TOKEN="[PAD]"\
+ OOV_TOKEN="[UNK]"\
+ BATCH_SIZE=15\
+ VALIDATION_BATCH_SIZE=15\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+#  PRIOR_TYPE=$PRIOR_TYPE
+
+# Ontonotes english
+# tacl-eer_eng-o_nns_eer
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=eng-o\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/ontonotes5/processed_docs/english\
+ TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=dev.jsonl\
+ TEST_DATA=test.jsonl\
+ VOCAB_PATH=data/ontonotes5/processed_docs/roberta-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/roberta-base\
+ PAD_TOKEN="<pad>"\
+ OOV_TOKEN="<unk>"\
+ BATCH_SIZE=2\
+ VALIDATION_BATCH_SIZE=1\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+ PRIOR_TYPE=$PRIOR_TYPE
+
+
+# Ontonotes chinese - intially had 30 epochs
+# tacl-eer_chi_nns_eer
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=chi\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/ontonotes5/processed_docs/chinese\
+ TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=dev.jsonl\
+ TEST_DATA=test.jsonl\
+ VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
+ PAD_TOKEN="[PAD]"\
+ OOV_TOKEN="[UNK]"\
+ BATCH_SIZE=2\
+ VALIDATION_BATCH_SIZE=1\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
+#  PRIOR_TYPE=$PRIOR_TYPE
+
+# Ontonotes arabic initially had 30 epochs
+# tacl-eer_ara_nns_eer
+bash scripts/run_ml_experiment.sh\
+ IS_REMOTE=$IS_REMOTE\
+ RUN_JUPYTER=$RUN_JUPYTER\
+ RUN_TENSORBOARD=$RUN_TENSORBOARD\
+ LANG_LABEL=ara\
+ DATASET_LABEL=$DATASET_LABEL\
+ METHOD_LABEL=$METHOD_LABEL\
+ BASE_CONFIG=experiments/supervised_tagger.jsonnet\
+ ASSUME_COMPLETE=$ASSUME_COMPLETE\
+ LANG_DIR=data/ontonotes5/processed_docs/arabic\
+ TRAIN_DATA=train$TRAIN_SUFFIX.jsonl\
+ DEV_DATA=dev.jsonl\
+ TEST_DATA=test.jsonl\
+ VOCAB_PATH=data/ontonotes5/processed_docs/mbert-entity.vocab\
+ MODEL_NAME=/gpfsdswork/dataset/HuggingFace_Models/bert-base-multilingual-cased\
+ PAD_TOKEN="[PAD]"\
+ OOV_TOKEN="[UNK]"\
+ BATCH_SIZE=2\
+ VALIDATION_BATCH_SIZE=1\
+ RANDOM_SEED=$RANDOM_SEED\
+ DROPOUT=$DROPOUT\
+ LR=$LR\
+ NUM_EPOCHS=$NUM_EPOCHS\
+ PRIOR_TYPE=$PRIOR_TYPE\
+ PRIOR_WEIGHT=$PRIOR_WEIGHT\
+ ENTITY_RATIO=$ENTITY_RATIO\
+ ENTITY_RATIO_MARGIN=$ENTITY_RATIO_MARGIN
 
