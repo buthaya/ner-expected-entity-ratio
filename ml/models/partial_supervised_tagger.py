12bf3be8db87bbf95a876eeaf582f8948b3e08c6
buthaya <uth.benno@gmail.com>
2024-05-13 16:20:01 +0200

    Debug JZ

--- ml/models/partial_supervised_tagger.py
+++ ml/models/partial_supervised_tagger.py
@@ -299,17 +299,6 @@ class PartialSupervisedTagger(Model):
             add_transitions=self.use_transitions,
         )
 
-        # ---------------------- Debugging ----------------------
-        print("labels, ", self.vocab.get_index_to_token_vocabulary(self.label_namespace))
-        print("tags[0,0], ", tags[0])
-        print("tags.shape, ", tags.shape)
-        print("self._constrain_potentials(tags, local_potentials)[0,0], ", self._constrain_potentials(tags, local_potentials)[0,0])
-        print("self._constrain_potentials(tags, local_potentials).shape, ", self._constrain_potentials(tags, local_potentials).shape)
-        print("constrained_pred_potentials[0,0], ", constrained_pred_potentials[0,0])
-        print("constrained_pred_potentials.shape, ", constrained_pred_potentials.shape)
-        print("local_potentials, ", local_potentials)
-        print("local_potentials.shape, ", local_potentials.shape)
-        # -------------------------------------------------------
         constrained_pred_crf = LinearChainCRF(constrained_pred_potentials, lengths=pred_crf.lengths)
         output["constrained_pred_crf"] = constrained_pred_crf
 
@@ -320,6 +309,21 @@ class PartialSupervisedTagger(Model):
             loss = loss + eer_loss
 
         output["loss"] = loss
+        # ---------------------- Debugging ----------------------
+        print("labels, ", self.vocab.get_index_to_token_vocabulary(self.label_namespace))
+        print("tags[0,0], ", tags[0])
+        print("tags.shape, ", tags.shape)
+        # print("self._constrain_potentials(tags, local_potentials)[0,0], ", self._constrain_potentials(tags, local_potentials)[0,0])
+        # print("self._constrain_potentials(tags, local_potentials).shape, ", self._constrain_potentials(tags, local_potentials).shape)
+        # print("constrained_pred_potentials[0,0], ", constrained_pred_potentials[0,0])
+        # print("constrained_pred_potentials.shape, ", constrained_pred_potentials.shape)
+        # print("local_potentials, ", local_potentials)
+        # print("local_potentials.shape, ", local_potentials.shape)
+        print("pred_crf.lengths, ", pred_crf.lengths)
+        print(constrained_pred_crf.marginals[0,0])
+        print(constrained_pred_potentials.sum(dim=2)[0,0])
+        # -------------------------------------------------------
+
         return output
 
     def calc_metrics(
